8e0d188e5239d671a6c52a87ab0366e00996e315
umelab <i0poti@mbr.nifty.com>
2024-02-11 14:04:18 +0900

    [upd]fix js

--- js/adogawa-current.js
+++ js/adogawa-current.js
@@ -19,6 +19,41 @@ $(document).ready(function() {
         },
         series: []
     };
+    $.get('../biwako-data/adogawa-current.csv', function(data2) {
+        // Split the lines
+         var lines = data2.split('\n');
+
+        // Iterate over the lines and add categories or series
+        $.each(lines, function(lineNo, line) {
+            var items = line.split(',');
+
+            // header line contains categories
+            if (lineNo == 0) {
+                $.each(items, function(itemNo, item) {
+                    options.xAxis.categories.push(item);
+                });
+            }
+            // the rest of the lines contain data with their name in the first position
+            else if (lineNo ==1){
+                var series = {
+                    name: '24時間前～現在',
+                    color: '#1E4083',
+                    dataLabels: {
+                        enabled: true
+                    },
+                    data: []
+                };
+
+                $.each(items, function(itemNo, item) {
+                    series.data.push(parseFloat(item));
+                });
+
+                options.series.push(series);
+            }
+        });
+        // Create the chart
+        var chart = new Highcharts.Chart(options);
+    });
 
     $.get('../biwako-data/adogawa-yesterday.csv', function(data1) {
          var lines = data1.split('\n');
@@ -56,42 +91,4 @@ $(document).ready(function() {
         var chart = new Highcharts.Chart(options);
     });
 
-    $.get('../biwako-data/adogawa-current.csv', function(data2) {
-        // Split the lines
-         var lines = data2.split('\n');
-
-        // Iterate over the lines and add categories or series
-        $.each(lines, function(lineNo, line) {
-            var items = line.split(',');
-
-            // header line contains categories
-            if (lineNo == 0) {
-                $.each(items, function(itemNo, item) {
-                    options.xAxis.categories.push(item);
-                });
-            }
-            // the rest of the lines contain data with their name in the first position
-            else if (lineNo ==1){
-                var series = {
-                    name: '24時間前～現在',
-                    color: '#1E4083',
-                    dataLabels: {
-                        enabled: true
-                    },
-                    data: []
-                };
-
-                $.each(items, function(itemNo, item) {
-                    series.data.push(parseFloat(item));
-                });
-
-                options.series.push(series);
-            }
-            else{
-            }
-        });
-
-        // Create the chart
-        var chart = new Highcharts.Chart(options);
-    });
 });
